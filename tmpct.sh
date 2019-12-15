25e442ed088ea914647522bc5ab2dac4823eac0f
kgor93 <mossme89@yahoo.com>
2019-12-15 12:58:06 -0500

    Update tmpct.sh

--- tmpct.sh
+++ tmpct.sh
@@ -1,5 +1,6 @@
 #usage: ./tmpct.sh <sleep time in seconds> KB|MB
 rm -rf tmscript*
+rm -rf tmscript.avg.txt
 mkdir -p tmscript
 touch ./tmscript/avg.txt
 touch ./tmscript/avgmb.txt
@@ -14,26 +15,37 @@ while true;
 
     iterations=$(wc -l tmscript/avg.txt | awk '{print $1}' | bc)
     fixediter=$(($iterations + 1))
-    echo "Total number of iterations is" $fixediter "and the time between is" $1 "seconds."
+    #echo "frequency" $frequency
+    frequency=$1
+
+    echo "Total number of iterations is" $fixediter "and the time between is" $frequency "seconds."
 
 	cat ./tmscript/tm.txt > ./tmscript/old.txt
 	cat ./tmscript/mb.txt > ./tmscript/mbold.txt
 
 	tmutil status | awk '/_raw_Percent/ {print $3}' | grep -o '[0-9].[0-9]\+' | awk '{print $1*100}' > ./tmscript/tm.txt
-	printf "The new percentage is " 
+	printf "The new percentage is "
 	cat ./tmscript/tm.txt  | tr '\n' '%'
         echo " "
-        
+
     if [ "$counter" -gt "0" ] ; then
 		printf "The difference is "
 		paste ./tmscript/tm.txt ./tmscript/old.txt | awk '{print $1 - $2}' >> ./tmscript/avg.txt
 		tail -1 ./tmscript/avg.txt  | tr '\n' '%'
                 echo " "
-		awk '{s+=$1} END {print "Average Difference: " s/NR}' ./tmscript/avg.txt | tr '\n' '%'
+        #newaverage=$(awk '{s+=$1} END {print "Average Difference: " s/NR}' ./tmscript/avg.txt | tr '\n' '%')
+        newaverage=$(awk '{s+=$1} END {print  s/NR}' ./tmscript/avg.txt)
+        echo "Average Difference: " $newaverage "%"
+
 	        #cat ./tmscript/avg.txt | tr '\n' ' '
                 echo " "
+		avgseconds=$(echo "scale=6;$newaverage / $frequency" | bc)
+		#echo "avgseconds" $avgseconds
+		avghours=$(echo "scale=6;100/ $avgseconds/ 3600" | bc)
+		echo "Estimated time remaining (in hours): " $avghours
          else
 		echo "0" >> ./tmscript.avg.txt
+
     fi
         if [ "$2" == "MB" ]; then
 
